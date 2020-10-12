fce7a2babf9ca99d4dc93fd02dec7b173868b8c1
lucasfernande <lucas.fernandes6192@outlook.com>
2020-10-12 15:02:14 -0300

    arquivo .js e função para recuperar o tamanho da janela

---
+++ js/jogo.js
@@ -0,0 +1,10 @@
+// definindo altura e largura do palco do jogo
+
+var altura
+var largura
+
+function ajustaTamanhoJanela() {
+	var altura = window.innerHeight
+	var largura =  window.innerWidth
+	// console.log(altura, largura)
+}
